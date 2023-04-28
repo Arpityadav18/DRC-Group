1b7606a81e810004a11b3f2c2d3f846b2221a1ec
Arpit Yadav <112938376+Arpityadav18@users.noreply.github.com>
2023-04-28 15:43:03 +0530

    First Commint
    First Commit

--- js/main.js
+++ js/main.js
@@ -336,6 +336,27 @@ function main() {
         offset: 80
     });
 
+    //show more text
+
+    
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+
 
 	// Hide nav on click
   $(".navbar-nav li a").click(function (event) {
